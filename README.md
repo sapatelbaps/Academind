0b0c4eaeae78c1afe6401a576a923ec8839b5d31
sapatelbaps <sapatelbaps@gmail.com>
2019-04-12 21:28:37 -0700

    Tested ES6 functions: forEach Filter Sort Map - unable to test in plain html wout gulp process/angular project/server Reduce - pending.

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # Academind
 
-"Angular 2.0 Final - Getting Started" playlist. 
-https://www.youtube.com/playlist?list=PL55RiY5tL51rLeDxRPlE6Hsnss3QDCCKc
+>"Angular 2.0 Final - Getting Started" playlist.
+
+>https://www.youtube.com/playlist?list=PL55RiY5tL51rLeDxRPlE6Hsnss3QDCCKc
+
+# JS Higher order functions and arrays:
+>forEach, filter, sort, map, reduce
+
+>Traversy Media youtube video:
+https://www.youtube.com/watch?v=rRgD1yVwIvE
---
+++ traversymedia_test.js
@@ -0,0 +1,69 @@
+const companies = [
+    {name: "Company One", category: "Finance", start: 1981, end: 2004},
+  {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+  {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+  {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+  {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+  {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+  {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+  {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+  {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+];
+
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
+
+// forEach - used for iteration.
+companies.forEach(function(company){
+    //console.log(company.name);
+});
+
+// filter - used to filter array.
+console.log("Tech companies");
+var techCompanies = companies.filter(company => company.category === "Technology");
+techCompanies.forEach(c => console.log(c.name));
+
+console.log("Companies after Nineties.");
+var recentCompanies = companies.filter(c=>c.start > 1990);
+recentCompanies.forEach(c=>console.log(c.start));
+
+console.log("Adult age numbers:");
+var adult = ages.filter(a=>a > 18);
+adult.forEach(m => console.log(m));
+
+// sort - used to sort array based on some property. Sorting on numeric array does not work as normally expected.
+console.log("Sorted Companies of nineties.");
+var companiesOfNineties = companies.sort(function(c1,c2){
+  if (c1.start > c2.start) {
+    return 1;
+  } else return -1;
+});
+companiesOfNineties.forEach(c=>console.log(c.start));
+
+// does not sort numbers as expected. Considers first digit only first time.
+var numSort1 = ages.sort();
+numSort1.forEach(n=>console.log(n));
+
+var numSort2 = ages.sort((a,b) => (a > b ? 1 : -1));
+numSort2.forEach(n=>console.log(n));
+
+// map - Used to generate new array based on existing array by applying any operation or conditions.
+var mappedArr1 = companies.map(function(company){
+  return company.name;
+});
+mappedArr1.forEach(item => console.log(item));
+
+var mappedArr2 = companies.map(function(company){
+  return company.category;
+});
+mappedArr2.forEach(item => console.log(item));
+
+console.log("Mapped Array 3");
+var mappedArr3 = companies.map(function(company){
+  return '${company.name} (${company.category}) : [${company.start} - ${company.end}]';
+});
+mappedArr3.forEach(item => console.log(item));
+
+const testMap = companies.map(function(company) {
+  return '${company.name} [${company.start} - ${company.end}]';
+});
+testMap.forEach(m => console.log(m));
